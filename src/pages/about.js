cdb0b0506025c53ba6ad4aeb918808b2501eeb7e
Sreejith V S <sreejithvs333@gmail.com>
2020-12-24 15:31:38 +0530

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+    </div>
+  );
+}
